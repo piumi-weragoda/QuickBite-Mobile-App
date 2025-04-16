3e259cecd7c4b8e4ab11f0efc0c5bf82cb9d2762
Piumi <piumiweragoda987@gmail.com>
2025-04-16 16:42:00 +0530

    Add HomeScreen and RestaurantScreen

--- QuickBite/App.js
+++ QuickBite/App.js
@@ -1,20 +1,10 @@
 import { StatusBar } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className ="text-red-500 text 4xl">Quick Bite</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-  },
-});
---
+++ QuickBite/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text, View } from "react-native";
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+}
